1c01b2cc7d0f7e6d38aa11c7ab72ce3c300e59dd
Pauuukin <oleg.paukin@mail.ru>
2019-11-08 13:12:12 +0300

    add new view for admin module

--- myfirst/myfirst/settings.py
+++ myfirst/myfirst/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 """список установленных пакетов"""
 INSTALLED_APPS = [
     'articles.apps.ArticlesConfig',
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -70,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request'
             ],
         },
     },
@@ -111,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 """время"""
 TIME_ZONE = 'UTC'
@@ -127,3 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
